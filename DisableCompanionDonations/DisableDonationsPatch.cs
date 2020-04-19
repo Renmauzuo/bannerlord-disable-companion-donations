6ba1d3278db23dcf65d7374e1727aef0c692faf0
Renmauzuo <renmauzuo@yahoo.com>
2020-04-18 20:16:25 -0500

    Initial Commit
    Initial commit of Disable Companion Donations mod. Disables companions in the player's clan from donating troops to settlement garrisons.

---
+++ DisableCompanionDonations/DisableDonationsPatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+
+
+namespace DisableCompanionDonations
+{
+    //It would be less intrusive to patch FindNumberOfTroopsToLeaveToGarrison in DefaultSettlementGarrisonModel instead but unfortunately that is currently not public
+    [HarmonyPatch(typeof(GarrisonTroopsCampaignBehavior), "OnSettlementEntered")]
+    class DisableDonationsPatch
+    {
+        static bool Prefix(MobileParty mobileParty, Settlement settlement, Hero hero)
+        {
+            if (mobileParty!= null && mobileParty.LeaderHero != null && mobileParty.LeaderHero.Clan == Clan.PlayerClan)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ DisableCompanionDonations/SubModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+
+namespace DisableCompanionDonations
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad() {
+            base.OnSubModuleLoad();
+
+            try
+            {
+                new Harmony("mod.DisableCompanionDonations.bannerlord").PatchAll();
+            }
+            catch (Exception ex)
+            {
+                int num = (int)MessageBox.Show("Error patching:\n" + ex.Message + " \n\n" + ex.InnerException?.Message);
+            }
+        }
+    }
+}
